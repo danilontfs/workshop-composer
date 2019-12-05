1e314bf9455af88cb1eb6093b98cad9e138502a4
Danilo Rocha <danilontfs@gmail.com>
2019-12-05 15:08:01 -0300

    Executa codesniffer

--- src/Soma.php
+++ src/Soma.php
@@ -2,10 +2,10 @@
 
 namespace PHPConf;
 
-class Soma{
-
-  public function calcula($a,$b){
-    return $a + $b;
-  }
-
+class Soma
+{
+    public function calcula($a, $b)
+    {
+        return $a + $b;
+    }
 }
